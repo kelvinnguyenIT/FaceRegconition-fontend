70c986206f70714a5897a72a353da6714a6a6149
NamKozo <namnv@kozo-japan.com>
2023-01-01 12:09:15 +0700

    Nam-update Contact page frontend

--- src/app/containers/Contact/index.jsx
+++ src/app/containers/Contact/index.jsx
@@ -8,10 +8,12 @@ export function Contact() {
               <div className='grow-0 shrink-0 basis-auto block w-full lg:flex lg:w-6/12 xl:w-4/12'>
                 <div className='map-container-2 w-full h-[500px]'>
                   <iframe
-                    src='https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed'
-                    className='left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg'
-                    frameborder='0'
-                    allowfullscreen
+                    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3835.7332974702185!2d108.25016631412404!3d15.975298246193393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142108997dc971f%3A0x1295cb3d313469c9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiB2w6AgVHJ1eeG7gW4gdGjDtG5nIFZp4buHdCAtIEjDoG4!5e0!3m2!1svi!2s!4v1672548953768!5m2!1svi!2s'
+                    allowFullScreen=''
+                    loading='lazy'
+                    height='500px'
+                    width='100%'
+                    referrerPolicy='no-referrer-when-downgrade'
                   ></iframe>
                 </div>
               </div>
@@ -42,8 +44,8 @@ export function Contact() {
                         <p className='font-bold mb-1 text-white'>
                           Technical support
                         </p>
-                        <p className='text-[#687382]'>support@example.com</p>
-                        <p className='text-[#687382]'>+1 234-567-89</p>
+                        <p className='text-[#687382]'>lvtan.18it1@gmail.com</p>
+                        <p className='text-[#687382]'>(+84) 789-654-521</p>
                       </div>
                     </div>
                   </div>
@@ -55,25 +57,25 @@ export function Contact() {
                             aria-hidden='true'
                             focusable='false'
                             data-prefix='fas'
-                            data-icon='dollar-sign'
-                            className='w-3 text-white'
+                            data-icon='headset'
+                            className='w-5 text-white'
                             role='img'
                             xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 288 512'
+                            viewBox='0 0 512 512'
                           >
                             <path
                               fill='currentColor'
-                              d='M209.2 233.4l-108-31.6C88.7 198.2 80 186.5 80 173.5c0-16.3 13.2-29.5 29.5-29.5h66.3c12.2 0 24.2 3.7 34.2 10.5 6.1 4.1 14.3 3.1 19.5-2l34.8-34c7.1-6.9 6.1-18.4-1.8-24.5C238 74.8 207.4 64.1 176 64V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48h-2.5C45.8 64-5.4 118.7.5 183.6c4.2 46.1 39.4 83.6 83.8 96.6l102.5 30c12.5 3.7 21.2 15.3 21.2 28.3 0 16.3-13.2 29.5-29.5 29.5h-66.3C100 368 88 364.3 78 357.5c-6.1-4.1-14.3-3.1-19.5 2l-34.8 34c-7.1 6.9-6.1 18.4 1.8 24.5 24.5 19.2 55.1 29.9 86.5 30v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-48.2c46.6-.9 90.3-28.6 105.7-72.7 21.5-61.6-14.6-124.8-72.5-141.7z'
+                              d='M192 208c0-17.67-14.33-32-32-32h-16c-35.35 0-64 28.65-64 64v48c0 35.35 28.65 64 64 64h16c17.67 0 32-14.33 32-32V208zm176 144c35.35 0 64-28.65 64-64v-48c0-35.35-28.65-64-64-64h-16c-17.67 0-32 14.33-32 32v112c0 17.67 14.33 32 32 32h16zM256 0C113.18 0 4.58 118.83 0 256v16c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-16c0-114.69 93.31-208 208-208s208 93.31 208 208h-.12c.08 2.43.12 165.72.12 165.72 0 23.35-18.93 42.28-42.28 42.28H320c0-26.51-21.49-48-48-48h-32c-26.51 0-48 21.49-48 48s21.49 48 48 48h181.72c49.86 0 90.28-40.42 90.28-90.28V256C507.42 118.83 398.82 0 256 0z'
                             ></path>
                           </svg>
                         </div>
                       </div>
                       <div className='grow ml-6'>
                         <p className='font-bold mb-1 text-white'>
-                          Sales questions
+                          Technical support
                         </p>
-                        <p className='text-[#687382]'>sales@example.com</p>
-                        <p className='text-[#687382]'>+1 234-567-89</p>
+                        <p className='text-[#687382]'>nvnam.18it1@gmail.com</p>
+                        <p className='text-[#687382]'>(+84) 837-481-485</p>
                       </div>
                     </div>
                   </div>
@@ -99,9 +101,13 @@ export function Contact() {
                         </div>
                       </div>
                       <div className='grow ml-6'>
-                        <p className='font-bold mb-1 text-white'>Press</p>
-                        <p className='text-[#687382]'>press@example.com</p>
-                        <p className='text-[#687382]'>+1 234-567-89</p>
+                        <p className='font-bold mb-1 text-white'>
+                          Collaborator Contact
+                        </p>
+                        <p className='text-[#687382]'>
+                          collaborator@vku.udn.vn
+                        </p>
+                        <p className='text-[#687382]'>(+84).236.3667117</p>
                       </div>
                     </div>
                   </div>
@@ -128,8 +134,10 @@ export function Contact() {
                       </div>
                       <div className='grow ml-6'>
                         <p className='font-bold mb-1 text-white'>Bug report</p>
-                        <p className='text-[#687382]'>bugs@example.com</p>
-                        <p className='text-[#687382]'>+1 234-567-89</p>
+                        <p className='text-[#687382]'>
+                          supportcenter@vku.udn.vn
+                        </p>
+                        <p className='text-[#687382]'>(+84).236.3667117</p>
                       </div>
                     </div>
                   </div>
